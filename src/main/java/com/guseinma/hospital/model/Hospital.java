7abeaee02c329006dd8a6f748e950a5b76932f5a
etogus <guseynmamedov99@gmail.com>
2024-07-07 14:13:27 +0200

    getters/setters

--- src/main/java/com/guseinma/hospital/model/Hospital.java
+++ src/main/java/com/guseinma/hospital/model/Hospital.java
@@ -16,4 +16,44 @@ public class Hospital {
 
     @ManyToMany(mappedBy = "hospitals")
     private Set<Patient> patients = new HashSet<>();
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public Set<Patient> getPatients() {
+        return patients;
+    }
+
+    public void setPatients(Set<Patient> patients) {
+        this.patients = patients;
+    }
 }
--- src/main/java/com/guseinma/hospital/model/Patient.java
+++ src/main/java/com/guseinma/hospital/model/Patient.java
@@ -23,4 +23,60 @@ public class Patient {
             joinColumns = @JoinColumn(name = "patient_id"),
             inverseJoinColumns = @JoinColumn(name = "hospital_id"))
     private Set<Hospital> hospitals = new HashSet<>();
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public Set<Hospital> getHospitals() {
+        return hospitals;
+    }
+
+    public void setHospitals(Set<Hospital> hospitals) {
+        this.hospitals = hospitals;
+    }
 }
